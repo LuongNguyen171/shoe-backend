13a2f29242314e29935c80138bcc97d0a8f8a837
LuongNguyen171 <luongnguyen170102@gmail.com>
2023-09-09 10:40:51 +0700

    change 2

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const userModel = require('../models/auth.model')
 const token = require('../middlewares/token');
 
